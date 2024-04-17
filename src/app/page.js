0b1c3807be528755597c4964856531d8faab1eb1
Farok-Hossain <farokafs7@gmail.com>
2024-04-17 19:01:37 +0600

    link added

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import Link from "next/link";
 import React from "react";
 
 const page = () => {
   return (
     <div>
       <h1>This is home page</h1>
+      <Link to="/about">click here</Link>
     </div>
   );
 };
