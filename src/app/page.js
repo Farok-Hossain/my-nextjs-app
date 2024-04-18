4d0a9ed15dbb1a72f6400aee2adb35cea546df2a
Farok-Hossain <farokafs7@gmail.com>
2024-04-18 22:34:12 +0600

    updated home page

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ const page = () => {
   return (
     <div>
       <h1>This is home page</h1>
-      <h1 className=" text-5xl">This is home page</h1>
+      <h1 className=" text-7xl">This is home page</h1>
     </div>
   );
 };
