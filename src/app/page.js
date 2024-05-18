5cf7fe78b8b451483f58c58b986c359b6e47b9c6
Farok-Hossain <farokafs7@gmail.com>
2024-05-18 22:38:27 +0600

    updated

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ const page = () => {
     <div>
       <h1>This is home page</h1>
       <h1 className=" text-5xl text-pink-600">This is homes page</h1>
+      <button className=" btn btn-secondary">Click here</button>
     </div>
   );
 };
