b2b8216c145c6ab2c2584f00cd3296665534466d
Farok-Hossain <farokafs7@gmail.com>
2024-06-12 19:30:03 +0600

    updated

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,8 @@ import React from "react";
 const page = () => {
   return (
     <div>
-      <h1>This is home page</h1>
-      <h1 className=" text-5xl text-pink-600">This is homes page</h1>
+      <h1>This is Home Page</h1>
+      <h1 className=" text-5xl text-pink-500">This is homes page</h1>
       <button className=" btn btn-secondary">Click here</button>
     </div>
   );
