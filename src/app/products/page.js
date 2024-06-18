77e837023ed1c66e71a3f160249d61870920b381
Farok-Hossain <farokafs7@gmail.com>
2024-06-18 11:33:56 +0600

    updated

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -3,8 +3,12 @@ import React from "react";
 const ProductsPage = () => {
   return (
     <div>
-      <h1>This is product page</h1>
+      <h1>This is the ProductPage</h1>
       <p>All products are here:</p>
+      <li>List-1:</li>
+      <li>List-2:</li>
+      <li>List-3:</li>
+      <li>List-4:</li>
     </div>
   );
 };
