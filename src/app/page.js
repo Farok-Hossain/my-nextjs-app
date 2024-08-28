dc65f631628dc654ec0ed1bcda557659f4f4b044
Farok-Hossain <farokafs7@gmail.com>
2024-08-28 20:47:06 +0600

    page updated

--- src/app/page.js
+++ src/app/page.js
@@ -4,9 +4,10 @@ import React from "react";
 const page = () => {
   return (
     <div>
-      <h1>This is Home Page</h1>
       <h1 className=" text-5xl text-pink-500">This is homes page</h1>
-      <button className=" btn btn-secondary">Click here</button>
+      <Link href="https://www.youtube.com/" target="_blank">
+        <button className=" btn btn-secondary">Click here</button>
+      </Link>
     </div>
   );
 };
